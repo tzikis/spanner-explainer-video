6a7f236a83fc67b4f2156ea0b1558ba97dd2c3fc
Vasilis Georgitzikis <tzikis@gmail.com>
2019-07-03 20:01:38 +0300

    Sending Commands to the Lamp

--- test-script.py
+++ test-script.py
@@ -22,6 +22,30 @@ INPUT_PIN_GREEN = "A2"
 INPUT_PIN_BLUE = "A1"
 INPUT_PIN_WHITE = "A0"
 
+def send_cmd(command, value):
+    BASE_URL = 'api.particle.io'
+    API_PATH = '/v1/devices/'
+
+    resource_uri = API_PATH + device_id + '/' + command
+
+    conn = http.client.HTTPSConnection(BASE_URL)
+    headers = {'Authorization': 'Bearer ' + particle_token, "Content-type": "application/x-www-form-urlencoded"}
+    params = urllib.parse.urlencode({'@arg': value})
+    conn.request('POST', resource_uri, params, headers)
+
+    response = conn.getresponse()
+    response = response.read().decode()
+    json_obj = json.loads(response)
+    return json_obj["return_value"]
+
+def set_lamp_color(color):
+    print("$$$$ Setting Device Color $$$$")
+    print("New Color: " + color)
+    return_code = send_cmd("setColor", color)
+    if return_code != 1:
+        pytest.fail("Failed to set device color")
+    print("$$$$   Successfully Set   $$$$")
+
 def turn_device_on():
     print ("++++ Turning Device On ++++")
     testboard.digitalWrite(RELAY_PIN, 'HIGH')
@@ -34,6 +58,10 @@ def turn_device_off():
 
 def testDevice():
     turn_device_on()
-    time.sleep(5)
+    time.sleep(15) # Wait for the device to connect to the Wifi
+    set_lamp_color("ff000000") # Make the lamp Red
+    set_lamp_color("00ff0000") # Make the lamp Green
+    set_lamp_color("0000ff00") # Make the lamp Blue
+    set_lamp_color("000000ff") # Make the lamp White
     turn_device_off()
     assert True
